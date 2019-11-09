2e64bcd073c8e9b18ec8efda74abdbfcf944731c
Sheikh Deep <30627010+sheikhDeep@users.noreply.github.com>
2019-11-09 20:10:21 +0600

    Add files via upload
    This is the template that I use for competitive programming.

---
+++ templete.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+
+typedef long long  ll;
+#define endl '\n'
+
+
+
+
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+
+const int MOD = 1000000007;
+
+//////////////////////////////////////////
+
+
+
+void Input() {
+
+}
+
+
+
+void Solve() {
+
+
+
+
+}
+
+int main() {
+
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  Input();
+  Solve();
+
+
+  return 0;
+
+}
